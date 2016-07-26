1c46272a95c6d5e92393344894330413d49e63bb
David SPORN <sporniket.studio@gmail.com>
2016-07-26 21:16:42 +0200

    implement basic event listener selection and event notification

--- src/main/java/com/sporniket/libre/p3/P3.java
+++ src/main/java/com/sporniket/libre/p3/P3.java
@@ -3,6 +3,9 @@
  */
 package com.sporniket.libre.p3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.sporniket.libre.io.parser.properties.MultipleLinePropertyParsedEvent;
 import com.sporniket.libre.io.parser.properties.PropertiesParsingListener;
 import com.sporniket.libre.io.parser.properties.SingleLinePropertyParsedEvent;
@@ -13,25 +16,79 @@ import com.sporniket.libre.io.parser.properties.SingleLinePropertyParsedEvent;
  */
 public class P3 implements PropertiesParsingListener
 {
+	/**
+	 * A <em>processor</em> is defined by the {@link PropertyNameMatcher} that select the properties for which it is appliable, and
+	 * the {@link PropertiesParsingListener} that implement the actual processing.
+	 * 
+	 * @author dsporn
+	 *
+	 */
+	private static class Processor
+	{
+		public final PropertiesParsingListener myListener;
 
-	/* (non-Javadoc)
-	 * @see com.sporniket.libre.io.parser.properties.PropertiesParsingListener#onMultipleLinePropertyParsed(com.sporniket.libre.io.parser.properties.MultipleLinePropertyParsedEvent)
+		public final PropertyNameMatcher myMatcher;
+
+		public Processor(PropertyNameMatcher matcher, PropertiesParsingListener listener)
+		{
+			myMatcher = matcher;
+			myListener = listener;
+		}
+
+		private PropertiesParsingListener getListener()
+		{
+			return myListener;
+		}
+
+		private PropertyNameMatcher getMatcher()
+		{
+			return myMatcher;
+		}
+	}
+	
+	private final List<Processor> myProcessors = new ArrayList<P3.Processor>() ;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.sporniket.libre.io.parser.properties.PropertiesParsingListener#onMultipleLinePropertyParsed(com.sporniket.libre.io.parser
+	 * .properties.MultipleLinePropertyParsedEvent)
 	 */
 	@Override
 	public void onMultipleLinePropertyParsed(MultipleLinePropertyParsedEvent event)
 	{
-		// TODO Auto-generated method stub
-
+		for (Processor _p : getProcessors())
+		{
+			if (_p.getMatcher().isMatching(event.getName()))
+			{
+				_p.getListener().onMultipleLinePropertyParsed(event);
+			}
+		}
 	}
 
-	/* (non-Javadoc)
-	 * @see com.sporniket.libre.io.parser.properties.PropertiesParsingListener#onSingleLinePropertyParsed(com.sporniket.libre.io.parser.properties.SingleLinePropertyParsedEvent)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.sporniket.libre.io.parser.properties.PropertiesParsingListener#onSingleLinePropertyParsed(com.sporniket.libre.io.parser
+	 * .properties.SingleLinePropertyParsedEvent)
 	 */
 	@Override
 	public void onSingleLinePropertyParsed(SingleLinePropertyParsedEvent event)
 	{
-		// TODO Auto-generated method stub
+		for (Processor _p : getProcessors())
+		{
+			if (_p.getMatcher().isMatching(event.getName()))
+			{
+				_p.getListener().onSingleLinePropertyParsed(event);
+			}
+		}
+	}
 
+	private List<Processor> getProcessors()
+	{
+		return myProcessors;
 	}
 
 }
